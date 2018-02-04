99d075bc1701574200a45cd6873955dd67b71579
Ryan Wright <rywright@gmail.com>
2018-02-04 13:17:59 -0800

    Initial SPEED commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 extern crate zip;
+extern crate time;
+extern crate regex;
 
+use std::time::Instant;
+use regex::RegexBuilder;
 use std::fs;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -24,25 +28,52 @@ fn parse_irccloud_log_file() {
 
     for i in 0..archive.len() {
         let mut file = archive.by_index(i).unwrap();
-
         if !(&*file.name()).ends_with('/') {
+            let start = Instant::now();
+
             let mut content = String::new();
             file.read_to_string(&mut content);
 
-            if content.contains(search_phrase) {
+            if start.elapsed().as_secs() > 0 {
+                println!("read to string: {}", start.elapsed().as_secs());
+            }
+
+            let needle = RegexBuilder::new(search_phrase)
+                .case_insensitive(true)
+                .build()
+                .expect("Invalid Regex");
+
+            if needle.is_match(&content) {
+                println!("contains was true: {}", start.elapsed().as_secs());
+                let mut file_name = String::from(&*file.name());
+                //println!("content: {}", &content);
                 let mut file_name = String::from(&*file.name());
                 let mut split_file_name = file_name.split("/");
                 let vec = split_file_name.collect::<Vec<&str>>();
                 let network = &*vec[1].to_string();
                 let channel = &*vec[2].to_string();
 
+                let start = Instant::now();
+                println!("1network: {}\nchannel: {}, search_phrase: {}\n\n", network, channel, search_phrase);
+                println!("1search_phrase: {}\n\n", search_phrase);
+
                 let reader = BufReader::new(file);
                 for line in reader.lines() {
-                    let mut rawline: String = line.unwrap().to_lowercase();
-                    if rawline.contains(search_phrase) {
-                            println!("network: {}\nchannel: {}\nraw line: {}\n\n", network, channel, rawline);
+                    println!("in line looper: {}", start.elapsed().as_secs());
+
+                    let mut rawline: String = line.unwrap();
+                    if needle.is_match(&rawline) {
+                        println!("2network: {}\nchannel: {}, search_phrase: {}\nraw line: {}\n\n", network, channel, search_phrase, rawline);
                     }
                 }
+                println!("loop through each line finished: {}", start.elapsed().as_secs());
+            } else {
+                if start.elapsed().as_secs() > 0 {
+                    println!("contains was false: {}", start.elapsed().as_secs());
+                }
+            }
+            if start.elapsed().as_secs() > 0 {
+                println!("whole file: {}", start.elapsed().as_secs());
             }
         }
     }
